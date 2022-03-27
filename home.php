78e883b10cb05b1915297cd050613e524b57d51f
AndreasPlangger <andreas_plangger@hotmail.com>
2022-03-27 14:40:21 +0200

    edits

--- home.php
+++ home.php
@@ -24,16 +24,20 @@ $tbody = ''; //this variable will hold the body for the table
 if (mysqli_num_rows($result)  > 0) {
     while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
         $tbody .=
-            "<tr>
-            <td><img class='img-thumbnail' src='pictures/" . $row['picture'] . "'</td>
-            <td>" . $row['pet_name'] . "</td>
-            <td>" . $row['breed'] . "</td>
-            <td>" . $row['size'] . "</td>
-            <td>" . $row['age'] . "</td>
-            <td>" . $row['pet_description'] . "</td>
-            <td>" . $row['hobbies'] . "</td>
-            <td>" . "<a href='details.php?petID=" . $row['petID'] . "'><button class='btn btn-success' type='button'><span class='text-nowrap'>More Info</span></button></a></td>
-            </tr>";
+            "<div class='col'>
+                <div class='card h-100 shadow justify-content-center'>
+                    <img src='pictures/" . $row['picture'] . "'' class='card-img-top' alt='" . $row['pet_name'] . "'>
+                    <div class='card-body'>
+                    <h4 class='card-title text-center'>" . $row['pet_name'] . "</h5>
+                    <p class='card-text'>Breed: " . $row['breed'] . "   </p>
+                    <p class='card-text'>Size: " . $row['size'] . "   </p>
+                    <p class='card-text'>Age: " . $row['age'] . "   </p>
+                    <p class='card-text'>Hobbies: " . $row['hobbies'] . "   </p>
+                    <p class='card-text'>" . $row['pet_description'] . "   </p>
+                    <span class='d-flex justify-content-center '>" . "<a href='details.php?petID=" . $row['petID'] . "'><button class='btn btn-success' type='button'><span class='text-nowrap'>More Info</span></button></a></span>
+                    </div>
+                    </div>
+                </div>";
     };
 } else {
     $tbody =  "<tr><td colspan='5'><center>No Data Available </center></td></tr>";
@@ -51,14 +55,31 @@ mysqli_close($connect);
     <title>Adopt a PET!</title>
     <?php require_once 'components/boot.php' ?>
     <style type="text/css">
-        .userImage {
-            width: auto;
-            height: 100px;
+        @media (min-width: 1400px) {
+
+            .container,
+            .container-lg,
+            .container-md,
+            .container-sm,
+            .container-xl,
+            .container-xxl {
+                max-width: 1420px !important;
+            }
+
+            .userImage {
+                width: auto;
+                height: 100px;
+            }
+
+            .container {
+                width: 100% !important;
+
+            }
         }
 
         .hero {
-            width: 103%;
-            height: 23vh;
+            width: 100% !important;
+            height: 28vh;
             background: rgb(2, 0, 36);
             background: linear-gradient(90deg, rgba(2, 0, 36, 1) 0%, rgba(37, 201, 156, 1) 0%, rgba(0, 255, 181, 0.8337710084033614) 100%);
 
@@ -68,9 +89,9 @@ mysqli_close($connect);
             margin: auto;
         }
 
-        .img-thumbnail {
+        .card-img-top {
             width: auto !important;
-            height: 18vh !important;
+            height: 45vh !important;
         }
 
         td {
@@ -92,43 +113,29 @@ mysqli_close($connect);
 
 
 
-    <div class="manageProduct w-75 mt-5">
+    <div class="manageProduct w-75 mt-3">
 
         <div class="container m-0 p-0">
             <div class="hero d-flex flex-column align-items-center">
-                <p class="h1 mt-5 text-center">Hi <?php echo $rowu['first_name']; ?> ! </p>
-                <p class='h3 text-center' style='padding-top: 2vh; padding-bottom: 2vh;'>Meet your new best friend!</p>
+                <p class='h1 mt-5 text-center' style='padding-top: 2vh; padding-bottom: 1vh;'>Hi <?php echo $rowu['first_name']; ?> ! </p>
+                <p class='h3 text-center' style='padding-top: 1vh; padding-bottom: 2vh;'>Meet your new best friend!</p>
             </div>
-            <a href="logout.php?logout">Sign Out</a>
-            <a href="update.php?id=<?php echo $_SESSION['user'] ?>">Update your profile</a>
+
         </div>
-
-
-
-
-        <div class='mb-4 d-flex justify-content-end'>
-            <a href="senior.php"><button class='btn btn-primary' type="button">Show senior animals only</button></a>
-        </div>
-
-        <table class='table table-border table-striped'>
-            <thead class='table-success'>
-                <tr>
-                    <th style='padding-top: 2vh; padding-bottom: 2vh; font-size: large'>Picture</th>
-                    <th style='padding-top: 2vh; padding-bottom: 2vh; font-size: large'>Name</th>
-                    <th style='padding-top: 2vh; padding-bottom: 2vh; font-size: large'>Breed</th>
-                    <th style='padding-top: 2vh; padding-bottom: 2vh; font-size: large'>Size</th>
-                    <th style='padding-top: 2vh; padding-bottom: 2vh; font-size: large'>Age</th>
-                    <th style='padding-top: 2vh; padding-bottom: 2vh; font-size: large'>Description</th>
-                    <th style='padding-top: 2vh; padding-bottom: 2vh; font-size: large'>Hobbies</th>
-                    <th style='padding-top: 2vh; padding-bottom: 2vh; font-size: large'>Details</th>
-
-
-                </tr>
-            </thead>
+        <span class='d-flex'>
+            <div class='w-50 mb-4 d-flex justify-content-beginning' style='margin-top: 4vh; margin-bottom: 2vh;'>
+                <a href="logout.php?logout"><button class='btn btn-primary me-1 text-white' type="button">Sign Out</button></a>
+                <a href="update.php?id=<?php echo $_SESSION['user'] ?>"><button class='btn btn-primary text-white' type="button">Update your profile</button></a>
+            </div>
+            <div class=' w-50 mb-4 d-flex justify-content-end' style='margin-top: 4vh; margin-bottom: 2vh;'>
+                <a href="senior.php"><button class='btn btn-success text-white' type="button">Show senior animals only</button></a>
+            </div>
+        </span>
+        <div class='row row-cols-1 row-cols-md-3 row-cols-lg-4 g-5'>
             <tbody>
                 <?= $tbody; ?>
             </tbody>
-        </table>
+        </div>
     </div>
 
 </body>
